08fb62a02be7c0d256e7c5a6c73fd638402a9a2d
levanhuy1211 <levanhuy1211@gmail.com>
2024-03-06 22:03:18 +0700

    add

--- thiepnhatrai/a/db.php
+++ thiepnhatrai/a/db.php
@@ -1,6 +1,6 @@
 <?php
 @session_start();
-if(isset($_SESSION['login'])){
+if(isset($_COOKIE['login'])){
     $pdo = new PDO("mysql:host=10.3.48.70;dbname=huytamdb", "huytam_user", "ieFIMOufxb6dRHsPaAwt");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
