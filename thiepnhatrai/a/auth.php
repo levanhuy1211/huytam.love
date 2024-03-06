1245c19f9bfbf25234f1fcd4287aef93c54e1b24
levanhuy1211 <levanhuy1211@gmail.com>
2024-03-06 22:17:16 +0700

    add

--- thiepnhatrai/a/auth.php
+++ thiepnhatrai/a/auth.php
@@ -4,6 +4,8 @@
     <input type="submit" name="submit" value="Submit">
 </form>
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 @session_start();
 if (isset($_POST['submit'])) {
     if ($_POST['oneCode']=="huytam123") {
--- thiepnhatrai/a/db.php
+++ thiepnhatrai/a/db.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 @session_start();
 if(isset($_SESSION['login'])){
     $pdo = new PDO("mysql:host=10.3.48.70;dbname=huytamdb", "huytam_user", "ieFIMOufxb6dRHsPaAwt");
