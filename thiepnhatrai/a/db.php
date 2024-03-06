a26b321dcd6bbd9a8f4e2c0882b36bb2c6d90d2b
levanhuy1211 <levanhuy1211@gmail.com>
2024-03-06 22:18:29 +0700

    add

--- thiepnhatrai/a/db.php
+++ thiepnhatrai/a/db.php
@@ -5,7 +5,5 @@ ini_set('display_errors', 1);
 if(isset($_SESSION['login'])){
     $pdo = new PDO("mysql:host=10.3.48.70;dbname=huytamdb", "huytam_user", "ieFIMOufxb6dRHsPaAwt");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}else{
-  header("Location: auth.php");
 }
 ?>
