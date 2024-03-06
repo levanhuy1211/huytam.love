f7735b7ebb1f8675bdd95c0c0c20b8f31e3a2c10
levanhuy1211 <levanhuy1211@gmail.com>
2024-03-06 22:24:19 +0700

    add

--- thiepnhatrai/a/auth.php
+++ thiepnhatrai/a/auth.php
@@ -1,8 +1,3 @@
-
-<form method="POST">
-    <input type="text" name="oneCode" placeholder="Vd: 123123" required="required" class="form-control" id="verification_code">
-    <input type="submit" name="submit" value="Submit">
-</form>
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
@@ -13,9 +8,12 @@ if (isset($_POST['submit'])) {
         // Thiết lập header HTTP cho chuyển hướng
         header('Location: /index.php', true, 302);
         exit; // Đảm bảo rằng script dừng lại sau khi chuyển hướng
-
     } else {
         echo 'FAILED';
     }
 }
 ?>
+<form method="POST">
+    <input type="text" name="oneCode" placeholder="Vd: 123123" required="required" class="form-control" id="verification_code">
+    <input type="submit" name="submit" value="Submit">
+</form>
